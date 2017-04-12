a8295d5d542938dbfe2b9b500669da4dd4d2b7ab
LoreleiGab <loreleigab@hotmail.com>
2017-04-12 15:13:45 -0300

    Atualizações Contabilidade

---
+++ perfil/contabilidade/atualizacoes.php
@@ -0,0 +1,10 @@
+<section id="list_items" class="home-section bg-white">
+	<h2>Últimos 30 Uploads</h2>
+	<div class="container">
+		<div class="col-md-offset-2 col-md-8">
+			<div class="table-responsive list_info">
+				<?php listarAtualizacoes();?>
+			</div>
+		</div>
+	</div>
+</section> 
--- perfil/contabilidade/includes/funcoesContabilidade.php
+++ perfil/contabilidade/includes/funcoesContabilidade.php
@@ -1,4 +1,26 @@
 <?php 
 	$con = bancoMysqli();
-			
+	function listarAtualizacoes()
+	{
+		$con = bancoMysqli();
+		$sql = "SELECT * FROM atualizacao ORDER BY 'id' LIMIT 0,29";
+		$query = mysqli_query($con,$sql);
+		echo
+			"<table class='table table-condensed'>
+				<thead>
+					<tr class='list_menu'>
+						<td align='center'>Texto</td>
+					</tr>
+				</thead>
+				<tbody>";
+		while($campo = mysqli_fetch_array($query))
+		{
+			echo
+					"<tr>
+						<td align='center' class='list_description'>".$campo['texto']."</td>
+					</tr>";
+		}
+		echo "</tbody>
+			</table>";
+	}
 ?>
